d211bcb29fea0491d389ac90a6fde98ce82b1423
zhb123456789 <zhb123456789@users.noreply.github.com>
2020-09-27 18:31:49 +0800

    1

--- app/src/main/java/cn/com/yafo/yafopda/Adapter/SnOrderEntryAdapter.kt
+++ app/src/main/java/cn/com/yafo/yafopda/Adapter/SnOrderEntryAdapter.kt
@@ -1,7 +1,9 @@
 package cn.com.yafo.yafopda.Adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -71,7 +73,7 @@ class SnOrderEntryAdapter(
             binding = DataBindingUtil.getBinding(convertView)
         }
         binding!!.setVariable(BR.sn, data.snList[position])
-        binding.snOrderEntryItemLayout.setOnClickListener(OnItemClickListener(position))
+        binding.btDel.setOnClickListener(OnDelClickListener(position))
         //https://segmentfault.com/a/1190000008246487  binding.setVariable(variableId, data.get(position));
         return binding.root
     }
@@ -83,21 +85,13 @@ class SnOrderEntryAdapter(
              if (data.ncChkNum.value!! > data.checkedNum.value!!)
              {
                  data.snList.add(snStr)
-                 var checknum=data.checkedNum.value
-                 if (checknum == null) {
-                     checknum=1
-                 }
-                 else
-                 {checknum +=1 }
-                 data.checkedNum.postValue(checknum)
+                 data.checkedNum.postValue(data.snList.size)
 
                  notifyDataSetChanged()
              }
              else
              {
-                 val t = Toast.makeText(context, "数量超出", Toast.LENGTH_LONG)
-                 t.setGravity(Gravity.TOP, 0, 0)
-                 t.show()
+                 Toast.makeText(context, "序列号数量超出", Toast.LENGTH_LONG).show()
                  BeeAndVibrateManager.playBeeAndVibrate(context,R.raw.warning ,100,null)
 
              }
@@ -105,6 +99,7 @@ class SnOrderEntryAdapter(
          }
          else
          {
+             Toast.makeText(context, "序列号重复", Toast.LENGTH_LONG).show()
              //提示音
              BeeAndVibrateManager.playBeeAndVibrate(context,R.raw.serialnumberduplicate ,500,null)
          }
@@ -116,14 +111,35 @@ class SnOrderEntryAdapter(
         }
     }
 
-    inner class OnItemClickListener(private val position: Int) : View.OnClickListener {
+    inner class OnDelClickListener(private val position: Int) : View.OnClickListener {
         override fun onClick(view: View) {
             try {
                 val bundle = Bundle()
                 bundle.putInt("position", position)
+                // Toast.makeText(context,position, Toast.LENGTH_LONG).show();
+                //提示确认
+                val dialog =
+                    AlertDialog.Builder(context)
+                dialog.setTitle("确认")
+                // dialog.setIcon(R.drawable.dictation2_64);
+                dialog.setMessage("删除此序列号？")
 
+                // 设置“确定”按钮,使用DialogInterface.OnClickListener接口参数
+                dialog.setPositiveButton(
+                    "确定"
+                ) { _, _ ->
+                    Log.d("Dialog", "点击了“确认”按钮")
+                    data.snList.remove(data.snList[position])
+                    data.checkedNum.postValue(data.snList.size)
+                    notifyDataSetChanged() //刷新数据
+                }
+
+                // 设置“取消”按钮,使用DialogInterface.OnClickListener接口参数
+                dialog.setNegativeButton(
+                    "取消"
+                ) { _, _ -> Log.d("Dialog", "点击了“取消”按钮") }
+                dialog.show()
                 notifyDataSetChanged() //刷新数据
-
             } catch (e: Exception) {
                 Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show()
             }
